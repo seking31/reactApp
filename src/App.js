6633bde93321f553efccccd1efdb2c0456a96086
SaraKing <sking@vertafore.com>
2017-12-06 13:02:40 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+var axios = require('axios');
+
+const Card = (props) => {
+    return (
+        <div>
+            <div style={{ color: 'blue' }}>
+                <img src={props.avatar_url} />
+                <div>{props.name} </div>
+                <div>{props.company} </div>
+            </div>
+        </div>
+    );
+};
+
+const CardList = (props) => {
+    return (
+        <div>
+            {props.cards.map(card => <Card {...card} />)}
+        </div>
+    )
+}
+
+class Form extends React.Component {
+    state = { userName: '' }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios.get(`https://api.github.com/users/${this.state.userName}`)
+            .then(resp => {
+                this.props.onSubmit(resp.data);
+                this.setState({ userName: '' })
+            })
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text"
+                    value={this.state.userName}
+                    onChange={(event) => this.setState({ userName: event.target.value })}
+                    placeholder="username" />
+                <button type="submit">Add Card</button>
+            </form>
+        )
+    }
+}
+
+class App extends React.Component {
+    state = {
+        cards: [
+            {
+                name: "Jane",
+                avatar_url: "https://avatars2.githubusercontent.com/u/20879359?v=4",
+                company: "Vertafore"
+            },
+            {
+                name: "Chris",
+                avatar_url: "https://avatars2.githubusercontent.com/u/20879359?v=7",
+                company: "Panda"
+            }
+        ]
+    };
+
+    addNewCard = (cardInfo) => {
+        this.setState(prevState => ({
+            cards: prevState.cards.concat(cardInfo)
+        }))
+    };
+
+    render() {
+        return (
+            <div>
+                <Form onSubmit={this.addNewCard} />
+                <div></div>
+                <CardList cards={this.state.cards} />
+            </div>
+        )
+    }
+}
+
+export default App;
